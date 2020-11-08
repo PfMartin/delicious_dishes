178179a194e46da02395c35fa0f3652f1b3376e1
Martin <martin@Debian-10-4-0.Home>
2020-11-08 20:21:27 +0100

    added a site state

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,33 @@ import React, { Fragment } from 'react';
 import './App.css'
 
 import CategoryBar from './components/CategoryBar.js';
-import RecipeList from './components/RecipeList.js'
+import Footer from './components/Footer.js';
+import RecipeList from './components/RecipeList.js';
+import AddRecipe from './components/AddRecipe.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      site: 'AddRecipe',
+    };
   }
 
   render() {
+
+    let site;
+
+    if (this.state.site === 'AddRecipe') {
+      site = <AddRecipe />
+    } else if (this.state.site === 'RecipeList') {
+      site = <RecipeList />
+    }
+
     return(
       <Fragment>
         <CategoryBar />
-        <RecipeList />
+        {site}
+        <Footer />
       </Fragment>
     )
   }
