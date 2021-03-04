ea2a039fad006bdae785f228d990d43d3bc6e7fb
PfMartin <martinpfatrisch@gmail.com>
2021-03-04 18:01:37 +0100

    Production server for serving the react app

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('client/build'));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
+
+const port = process.env.PORT || 3000;
+const host = '192.168.178.26';
+
+app.listen(port, host);
